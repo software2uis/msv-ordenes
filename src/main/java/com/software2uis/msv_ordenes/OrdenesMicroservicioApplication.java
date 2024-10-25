6452d7e8f181ae521363400d9956c7268463f29a
Minerisho <migueplata5@gmail.com>
2024-10-25 14:56:13 -0500

    Inicialización del proyecto Spring Boot

---
+++ src/main/java/com/software2uis/msv_ordenes/OrdenesMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.software2uis.msv_ordenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdenesMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdenesMicroservicioApplication.class, args);
+	}
+
+}
